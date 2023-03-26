518e6abf2baacfaa7b653ef393140d1a8036038a
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-03-26 02:23:12 -0600

    inyeccion con Autowired

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -5,21 +5,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 
 @Controller
 public class IndexController {
 	
 	//creacion de un objeto, alto acoplamiento dificil de mantener
 	//private MiServicio servicio = new MiServicio();
-	//ahora se esta inyectando la instancia
+	//ahora se esta inyectando la instancia pero se puede desacoplar mas usando una interfaz
+	//private MiServicio servicio;
 	@Autowired
-	private MiServicio servicio;
+	private IServicio servicio;
 	
+	//inyectando a travez del contructor. el mismo principio
+	//para realizar la inyeccion se quedan a modo de ejemplo
+	/* Se puede omitir el Autowired y funciona igual
+	@Autowired
+	public IndexController(IServicio servicio) {
+		this.servicio = servicio;
+	}*/
+
 	//mapeo a un arreglo de rutas
 	@GetMapping({"/index","/",""})
 	public String index(Model model) {
 		model.addAttribute("objeto", servicio.operacion());
 		return "index";
 	}
+
+	//inyectar via metodo set, Se escribe la notacion @Autowired en el metodo set para que busque el objeto
+	//si ya se encuentra registrado en el contenedor de spring
+	/*@Autowired
+	public void setServicio(IServicio servicio) {
+		this.servicio = servicio;
+	}*/
+	
+	
 }
--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -8,8 +8,12 @@ import org.springframework.stereotype.Controller;
  *
  */
 //@Service
-@Controller
-public class MiServicio {
+@Controller("miServicioSimple")
+public class MiServicio implements IServicio{
+	
+	//todo componente spring tiene que tener un contructor sin argumentos
+	//para que no falle a la hora de agregar al contenedor de objectos
+	//Si no se usa manda error
 	
 	public String operacion() {
 		return "Ejecutando algun proceso importante con desacoplamiento";
