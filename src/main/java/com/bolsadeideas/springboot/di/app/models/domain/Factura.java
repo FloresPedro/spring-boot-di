5bcd9cb6bcbcbf6aaa57ce2a4d4b038dad4b5696
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-03-26 21:10:36 -0600

    Notaciones para despues de crear un bean y despues de destruirlo

--- src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -2,7 +2,11 @@ package com.bolsadeideas.springboot.di.app.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +18,26 @@ public class Factura {
 	@Autowired
 	private Cliente cliente;
 	@Autowired
+	@Qualifier("itemsFacturaOficina")
 	private List<ItemFactura> item;
-
+	
+	//modificar datos del componente o hacer algo justo despues de la contruccion del objeto
+	//inicializar recursos, objetos etc
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre( cliente.getNombre().concat(" ").concat("El Pedrin ") );
+		descripcion = descripcion.concat(" del cliente: ").concat(cliente.getNombre());
+	}
+	//despues de destruir el objeto del contenedor de spring
+	//podemos usarlo para cerrar recursos etc.
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(descripcion));
+	}
 	public String getDescripcion() {
 		return descripcion;
 	}
-
+	
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
